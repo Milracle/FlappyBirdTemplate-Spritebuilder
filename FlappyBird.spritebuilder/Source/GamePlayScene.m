12475c5da9a6e82431dace77c24f6917b5a2923b
Milan Mendpara <milanmendpara@ymail.com>
2015-06-12 20:31:05 +0530

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,9 +11,16 @@
 
 -(void)update:(CCTime)delta
 {
+    character = (Character *)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
     // put update code here
 }
 
 // put new methods here
+
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
 
 @end
